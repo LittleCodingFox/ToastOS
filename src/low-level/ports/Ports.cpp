aac76deac665c62d52b319b7baafa9a72ef80629
Nuno Silva <little.coding.fox@gmail.com>
2021-01-20 22:56:04 +0000

    Cleaned up Port code and added out/inport16/32

--- src/low-level/ports/Ports.cpp
+++ src/low-level/ports/Ports.cpp
@@ -7,13 +7,43 @@ void outport8(uint16_t port, uint8_t value)
 
 uint8_t inport8(uint16_t port)
 {
-    uint8_t returnVal;
+    uint8_t outValue;
 
     asm volatile ("inb %1, %0"
-    : "=a"(returnVal)
-    : "Nd"(port));
+        : "=a"(outValue)
+        : "Nd"(port));
 
-    return returnVal;
+    return outValue;
+}
+
+uint16_t inport16(uint16_t port)
+{
+    uint16_t outValue = 0;
+
+    asm volatile("inw %1, %0"
+        : "=a"(outValue)
+        : "Nd"(port));
+
+    return outValue;
+}
+
+void outport16(uint16_t port, uint16_t value)
+{
+  asm volatile("outw %0, %1" : : "a"(value), "Nd"(port));
+}
+
+uint32_t inport32(uint16_t port)
+{
+  uint32_t outValue = 0;
+
+  asm volatile ("inl %1, %0" : "=a"(outValue) : "Nd"(port));
+
+  return outValue;
+}
+
+void outport32(uint16_t port, uint32_t value)
+{
+  asm volatile ("outl %0, %1" : : "a"(value), "Nd"(port));
 }
 
 void io_wait()
--- src/low-level/ports/Ports.hpp
+++ src/low-level/ports/Ports.hpp
@@ -3,4 +3,11 @@
 
 void outport8(uint16_t port, uint8_t value);
 uint8_t inport8(uint16_t port);
+
+void outport16(uint16_t port, uint16_t value);
+uint16_t inport16(uint16_t port);
+
+void outport32(uint16_t port, uint32_t value);
+uint32_t inport32(uint16_t port);
+
 void io_wait();
