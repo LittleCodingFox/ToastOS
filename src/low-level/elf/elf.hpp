8b1244c9f619a31df16c457e3897ccc23d71c7a1
Nuno Silva <little.coding.fox@gmail.com>
2022-08-15 23:59:21 +0100

    Updated mlibc

--- src/low-level/elf/elf.hpp
+++ src/low-level/elf/elf.hpp
@@ -23,10 +23,21 @@ namespace Elf
 
     #define ELF_SECTION_FLAG_ALLOC          0x2
 
-    #define AT_ENTRY                        10
-    #define AT_PHDR                         20
-    #define AT_PHENT                        21
-    #define AT_PHNUM                        22
+    #define AT_NULL                         0
+    #define AT_IGNORE                       1
+    #define AT_EXECFD                       2
+    #define AT_PHDR                         3
+    #define AT_PHENT                        4
+    #define AT_PHNUM                        5
+    #define AT_PAGESZ                       6
+    #define AT_BASE                         7
+    #define AT_FLAGS                        8
+    #define AT_ENTRY                        9
+    #define AT_NOTELF                       10
+    #define AT_UID                          11
+    #define AT_EUID                         12
+    #define AT_GID                          13
+    #define AT_EGID                         14
 
     struct PACKED ElfHeader
     {
